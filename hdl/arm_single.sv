9cfd2c6ee86db364450e9b47dae8d18b21643f7e
Tyler Tucker <tylermatthewtucker@gmail.com>
2021-03-12 16:40:41 -0600

    Added some notes

--- hdl/arm_single.sv
+++ hdl/arm_single.sv
@@ -24,16 +24,16 @@
 //   Instr[19:16] = rn
 //   Instr[15:12] = rd
 //   Instr[11:8]  = 0000
-//   Instr[7:0]   = imm8      (for #immediate type) / 
+//   Instr[7:0]   = imm8      (for #immediate type) /
 //                  {0000,rm} (for register type)
-//   
+//
 // Load/Store instructions
 //   LDR, STR
 //   INSTR rd, [rn, #offset]
 //    LDR: rd <- Mem[rn+offset]
 //    STR: Mem[rn+offset] <- rd
 //   Instr[31:28] = cond
-//   Instr[27:26] = op = 01 
+//   Instr[27:26] = op = 01
 //   Instr[25:20] = funct
 //                  [25]:    0 (A)
 //                  [24:21]: 1100 (P/U/B/W)
@@ -82,12 +82,12 @@ module arm (input  logic        clk, reset,
             input  logic [31:0] ReadData,
             output logic        MemStrobe,
             input  logic        PCReady);
-   
+
    logic [3:0] ALUFlags;
    logic       RegWrite, ALUSrc, MemtoReg, PCSrc;
-   logic [2:0] RegSrc;   
+   logic [2:0] RegSrc;
    logic [1:0] ImmSrc, ALUControl;
-   
+
    controller c (.clk(clk),
                  .reset(reset),
                  .Instr(Instr[31:12]),
@@ -117,7 +117,7 @@ module arm (input  logic        clk, reset,
                 .WriteData(WriteData),
                 .ReadData(ReadData),
                 .PCReady(PCReady));
-   
+
 endmodule // arm
 
 module controller (input  logic         clk, reset,
@@ -126,15 +126,15 @@ module controller (input  logic         clk, reset,
                    output logic [ 2:0]  RegSrc,
                    output logic         RegWrite,
                    output logic [ 1:0]  ImmSrc,
-                   output logic         ALUSrc, 
+                   output logic         ALUSrc,
                    output logic [ 1:0]  ALUControl,
                    output logic         MemWrite, MemtoReg,
                    output logic         PCSrc,
                    output logic         MemStrobe);
-   
+
    logic [1:0] FlagW;
    logic       PCS, RegW, MemW;
-   
+
    decoder dec (.Op(Instr[27:26]),
                 .Funct(Instr[25:20]),
                 .Rd(Instr[15:12]),
@@ -170,7 +170,7 @@ module decoder (input  logic [1:0] Op,
                 output logic [1:0] ImmSrc, ALUControl,
                 output logic [2:0] RegSrc,
                 output logic       MemStrobe);
-   
+
    logic [11:0] controls;
    logic        Branch, ALUOp;
 
@@ -181,8 +181,7 @@ module decoder (input  logic [1:0] Op,
        2'b00: if (Funct[5]) controls = 12'b0000_0101_0010;
        // Data processing register
          else controls = 12'b0000_0001_0010;
-       // LDR
-       2'b01: if (Funct[0]) controls = 12'b0000_1111_0001;
+       2'b01: if (Funct[0]) controls = 12'b0000_1111_0001; //LDR if
        // STR
          else controls = 12'b0100_1110_1001;
        // BL
@@ -195,19 +194,22 @@ module decoder (input  logic [1:0] Op,
 
    assign {RegSrc, ImmSrc, ALUSrc, MemtoReg,
           RegW, MemW, Branch, ALUOp, MemStrobe} = controls;
-      
-   // ALU Decoder             
+
+   // ALU Decoder
    always_comb
      if (ALUOp)
        begin                 // which DP Instr?
-         case(Funct[4:1]) 
+         case(Funct[4:1])
            4'b0100: ALUControl = 2'b00; // ADD
+           4'b0101: ALUControl = 2'b00; // ADD w/ carry
            4'b0010: ALUControl = 2'b01; // SUB
+           4'b0110: ALUControl = 2'b01; // SUB w/ carry
            4'b0000: ALUControl = 2'b10; // AND
            4'b1100: ALUControl = 2'b11; // ORR
+           4'b1010: ALUControl = 2'b01; // CMP
            default: ALUControl = 2'bx;  // unimplemented
          endcase
-         // update flags if S bit is set 
+         // update flags if S bit is set
          // (C & V only updated for arith instructions)
          FlagW[1]      = Funct[0]; // FlagW[1] = S-bit
          // FlagW[0] = S-bit & (ADD | SUB)
@@ -218,10 +220,10 @@ module decoder (input  logic [1:0] Op,
          ALUControl = 2'b00; // add for non-DP instructions
          FlagW      = 2'b00; // don't update Flags
        end
-   
+
    // PC Logic
    assign PCS  = ((Rd == 4'b1111) & RegW) | Branch;
-   
+
 endmodule // decoder
 
 module condlogic (input  logic       clk, reset,
@@ -230,7 +232,7 @@ module condlogic (input  logic       clk, reset,
                   input  logic [1:0] FlagW,
                   input  logic       PCS, RegW, MemW,
                   output logic       PCSrc, RegWrite, MemWrite);
-   
+
    logic [1:0] FlagWrite;
    logic [3:0] Flags;
    logic       CondEx;
@@ -246,7 +248,7 @@ module condlogic (input  logic       clk, reset,
                           .en(FlagWrite[0]),
                           .d(ALUFlags[1:0]),
                           .q(Flags[1:0]));
-   
+
    // write controls are conditional
    condcheck cc (.Cond(Cond),
                  .Flags(Flags),
@@ -255,18 +257,18 @@ module condlogic (input  logic       clk, reset,
    assign RegWrite  = RegW  & CondEx;
    assign MemWrite  = MemW  & CondEx;
    assign PCSrc     = PCS   & CondEx;
-   
+
 endmodule // condlogic
 
 module condcheck (input  logic [3:0] Cond,
                   input  logic [3:0] Flags,
                   output logic       CondEx);
-   
+
    logic neg, zero, carry, overflow, ge;
-   
+
    assign {neg, zero, carry, overflow} = Flags;
    assign ge = (neg == overflow);
-   
+
    always_comb
      case(Cond)
        4'b0000: CondEx = zero;             // EQ
@@ -286,7 +288,7 @@ module condcheck (input  logic [3:0] Cond,
        4'b1110: CondEx = 1'b1;             // Always
        default: CondEx = 1'bx;             // undefined
      endcase // case (Cond)
-   
+
 endmodule // condcheck
 
 module datapath (input  logic        clk, reset,
@@ -303,12 +305,12 @@ module datapath (input  logic        clk, reset,
                  output logic [31:0] ALUResult, WriteData,
                  input  logic [31:0] ReadData,
                  input  logic        PCReady);
-   
+
    logic [31:0] PCNext, PCPlus4, PCPlus8;
    logic [31:0] ExtImm, SrcA, SrcB, Result;
    logic [ 3:0]  RA1, RA2, RA3;
-   logic [31:0] RA4;   
-   
+   logic [31:0] RA4;
+
    // next PC logic
    mux2 #(32)  pcmux (.d0(PCPlus4),
                       .d1(Result),
@@ -343,7 +345,7 @@ module datapath (input  logic        clk, reset,
                        .d1(PCPlus4),
                        .s(RegSrc[2]),
                        .y(RA4));
-   
+
    regfile     rf (.clk(clk),
                    .we3(RegWrite),
                    .ra1(RA1),
@@ -352,7 +354,7 @@ module datapath (input  logic        clk, reset,
                    .wd3(RA4),
                    .r15(PCPlus8),
                    .rd1(SrcA),
-                   .rd2(WriteData)); 
+                   .rd2(WriteData));
    mux2 #(32)  resmux (.d0(ALUResult),
                        .d1(ReadData),
                        .s(MemtoReg),
@@ -369,99 +371,103 @@ module datapath (input  logic        clk, reset,
    alu         alu (.a(SrcA),
                     .b(SrcB),
                     .ALUControl(ALUControl),
+                    .carryIn(carryIn),
                     .Result(ALUResult),
                     .ALUFlags(ALUFlags));
 endmodule // datapath
 
-module regfile (input  logic        clk, 
-                input  logic        we3, 
-                input  logic [ 3:0] ra1, ra2, wa3, 
+module regfile (input  logic        clk,
+                input  logic        we3,
+                input  logic [ 3:0] ra1, ra2, wa3,
                 input  logic [31:0] wd3, r15,
                 output logic [31:0] rd1, rd2);
-   
+
    logic [31:0] rf[14:0];
-   
+
    // three ported register file
    // read two ports combinationally
    // write third port on rising edge of clock
    // register 15 reads PC+8 instead
-   
+
    always_ff @(posedge clk)
-     if (we3) rf[wa3] <= wd3;   
+     if (we3) rf[wa3] <= wd3;
 
    assign rd1 = (ra1 == 4'b1111) ? r15 : rf[ra1];
    assign rd2 = (ra2 == 4'b1111) ? r15 : rf[ra2];
-   
+
 endmodule // regfile
 
 module extend (input  logic [23:0] Instr,
                input  logic [ 1:0] ImmSrc,
                output logic [31:0] ExtImm);
-   
+
    always_comb
-     case(ImmSrc) 
+     case(ImmSrc)
        // 8-bit unsigned immediate
-       2'b00:   ExtImm = {24'b0, Instr[7:0]};  
-       // 12-bit unsigned immediate 
-       2'b01:   ExtImm = {20'b0, Instr[11:0]}; 
-       // 24-bit two's complement shifted branch 
-       2'b10:   ExtImm = {{6{Instr[23]}}, Instr[23:0], 2'b00}; 
+       2'b00:   ExtImm = {24'b0, Instr[7:0]};
+       // 12-bit unsigned immediate
+       2'b01:   ExtImm = {20'b0, Instr[11:0]};
+       // 24-bit two's complement shifted branch
+       2'b10:   ExtImm = {{6{Instr[23]}}, Instr[23:0], 2'b00};
        default: ExtImm = 32'bx; // undefined
      endcase // case (ImmSrc)
-   
+
 endmodule // extend
 
 module adder #(parameter WIDTH=8)
    (input  logic [WIDTH-1:0] a, b,
     output logic [WIDTH-1:0] y);
-   
+
    assign y = a + b;
-   
+
 endmodule // adder
 
 module flopenr #(parameter WIDTH = 8)
    (input  logic             clk, reset, en,
-    input  logic [WIDTH-1:0] d, 
+    input  logic [WIDTH-1:0] d,
     output logic [WIDTH-1:0] q);
 
    always_ff @(posedge clk, posedge reset)
      if (reset)   q <= 0;
      else if (en) q <= d;
-   
+
 endmodule // flopenr
 
 module flopr #(parameter WIDTH = 8)
    (input  logic             clk, reset,
-    input  logic [WIDTH-1:0] d, 
+    input  logic [WIDTH-1:0] d,
     output logic [WIDTH-1:0] q);
 
    // Reset has start of .text
    always_ff @(posedge clk, posedge reset)
      if (reset) q <= 0;
      else       q <= d;
-   
+
 endmodule // flopr
 
 module mux2 #(parameter WIDTH = 8)
-   (input  logic [WIDTH-1:0] d0, d1, 
-    input  logic             s, 
+   (input  logic [WIDTH-1:0] d0, d1,
+    input  logic             s,
     output logic [WIDTH-1:0] y);
 
    assign y = s ? d1 : d0;
-   
+
 endmodule // mux2
 
 module alu (input  logic [31:0] a, b,
             input  logic [ 1:0] ALUControl,
+            input  logic carryIn,
             output logic [31:0] Result,
             output logic [ 3:0] ALUFlags);
-   
+
    logic        neg, zero, carry, overflow;
    logic [31:0] condinvb;
    logic [32:0] sum;
-   
+
+   // check if we are adding or subtracting and set b accordingly
+   // if ALUControl[0] is 0 the add, if 1 then subtract
    assign condinvb = ALUControl[0] ? ~b : b;
-   assign sum = a + condinvb + ALUControl[0];
+   assign sum = a + condinvb + ALUControl[0] + carryIn;
 
    always_comb
      casex (ALUControl[1:0])
@@ -474,9 +480,9 @@ module alu (input  logic [31:0] a, b,
    assign neg      = Result[31];
    assign zero     = (Result == 32'b0);
    assign carry    = (ALUControl[1] == 1'b0) & sum[32];
-   assign overflow = (ALUControl[1] == 1'b0) & 
-                     ~(a[31] ^ b[31] ^ ALUControl[0]) & 
-                     (a[31] ^ sum[31]); 
+   assign overflow = (ALUControl[1] == 1'b0) &
+                     ~(a[31] ^ b[31] ^ ALUControl[0]) &
+                     (a[31] ^ sum[31]);
    assign ALUFlags = {neg, zero, carry, overflow};
-   
+
 endmodule // alu
