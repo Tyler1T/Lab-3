30c8fbd3badac4d494628eca74e2cd4e3ac8c360
Tyler Tucker <tylermatthewtucker@gmail.com>
2021-03-16 13:56:44 -0500

    Implemented shifter module but its not finished. Still need ROR and put it in DP

--- hdl/arm_single.sv
+++ hdl/arm_single.sv
@@ -387,6 +387,7 @@ module regfile (input  logic        clk,
                 input  logic        we3,
                 input  logic [ 3:0] ra1, ra2, wa3,
                 input  logic [31:0] wd3, r15,
+                input  logic [6:0] shift
                 output logic [31:0] rd1, rd2);
 
    logic [31:0] rf[14:0];
@@ -494,3 +495,19 @@ module alu (input  logic [31:0] a, b,
    assign ALUFlags = {neg, zero, carry, overflow};
 
 endmodule // alu
+
+module shifter(input logic [6:0] shiftIn,
+                input logic [31:0] dataIn,
+                output logic [31:0] dataOut);
+      logic [1:0] shiftType = shiftIn[1:0];
+
+      always_comb
+        case(shiftType)
+          2'b00: assign dataOut = dataIn << shiftIn[6:2];
+          2'b01: assign dataOut = dataIn >> shiftIn[6:2];
+          2'b10: assign dataOut = dataIn >>> shiftIn[6:2];
+          2'b11: //ROR
+        endcase
+
+
+endmodule // shifter
